f343b2f6be7ec3df15b4c8fc6c9767248492de91
Angel-AG <miniprobecat@hotmail.com>
2022-01-20 16:02:26 -0600

    Add shogi board geometries

--- js/ShogiGeos.js
+++ js/ShogiGeos.js
@@ -1,7 +1,8 @@
 import * as THREE from './three.js';
 
+// -------------------------
 // Shogi Piece
-
+// -------------------------
 const SHOGI_PIECE_SHAPE = new THREE.Shape([
   new THREE.Vector2(0, 0),
   new THREE.Vector2(4, 0),
@@ -24,4 +25,45 @@ const SHOGI_PIECE_GEO = new THREE.ExtrudeGeometry(
 SHOGI_PIECE_GEO.computeBoundingBox();
 SHOGI_PIECE_GEO.center();
 
-export { SHOGI_PIECE_GEO };
+// -------------------------
+// Shogi Board
+// -------------------------
+const SHOGI_BOARD_BOX_GEO = new THREE.BoxGeometry(65, 35, 65);
+SHOGI_BOARD_BOX_GEO.computeBoundingBox();
+SHOGI_BOARD_BOX_GEO.center();
+
+const SHOGI_BOARD_LEG_GEO = new THREE.BoxGeometry(5, 7, 5);
+SHOGI_BOARD_LEG_GEO.computeBoundingBox();
+SHOGI_BOARD_LEG_GEO.center();
+
+// prettier-ignore
+const verticesOfCube = [
+  -1, -1, -1,    1, -1, -1,    1,  1, -1,    -1,  1, -1,
+  -1, -1,  1,    1, -1,  1,    1,  1,  1,    -1,  1,  1,
+];
+// prettier-ignore
+const indicesOfFaces = [
+  2, 1, 0,    0, 3, 2,
+  0, 4, 7,    7, 3, 0,
+  0, 1, 5,    5, 4, 0,
+  1, 2, 6,    6, 5, 1,
+  2, 3, 7,    7, 6, 2,
+  4, 5, 6,    6, 7, 4,
+];
+
+const SHOGI_BOARD_BOT_GEO = new THREE.PolyhedronGeometry(
+  verticesOfCube,
+  indicesOfFaces,
+  8,
+  2
+);
+
+SHOGI_BOARD_BOT_GEO.computeBoundingBox();
+SHOGI_BOARD_BOT_GEO.center();
+
+export {
+  SHOGI_PIECE_GEO,
+  SHOGI_BOARD_BOX_GEO,
+  SHOGI_BOARD_LEG_GEO,
+  SHOGI_BOARD_BOT_GEO,
+};
