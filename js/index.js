7f0adaffd9da121701451fd9bef659fcc44b2146
Angel-AG <miniprobecat@hotmail.com>
2022-01-20 19:32:42 -0600

    Add shogi board to scene

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 import * as THREE from './three.js';
 import { OrbitControls } from './OrbitControls.js';
+import {
+  SHOGI_BOARD_BOX_GEO,
+  SHOGI_BOARD_LEG_GEO,
+  SHOGI_BOARD_BOT_GEO,
+} from './ShogiGeos.js';
 
 let scene, camera, renderer, controls;
+let shogiBoard;
+
+// TODO: Create final materials
+const material = new THREE.MeshBasicMaterial({ color: 0xfef9bc });
+const material2 = new THREE.MeshBasicMaterial({ color: 0x724109 });
+const material3 = new THREE.MeshBasicMaterial({ color: 0xfbe7d0 });
 
 init();
 animate();
@@ -26,8 +37,9 @@ function init() {
   scene.add(axesHelper);
 
   createFloor();
+  createShogiBoard();
 
-  camera.position.set(0, 20, 100);
+  camera.position.set(0, 20, 150);
 
   //
 
@@ -41,6 +53,22 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function translateMatrix(x, y, z) {
+  let mat = new THREE.Matrix4();
+  mat.makeTranslation(x, y, z);
+
+  return mat;
+}
+
+function reflectMatrix(x, y, z) {
+  let mat = new THREE.Matrix4();
+  mat.elements[0] = x;
+  mat.elements[5] = y;
+  mat.elements[10] = z;
+
+  return mat;
+}
+
 function createFloor() {
   const geometry = new THREE.PlaneGeometry(150, 150);
   const material = new THREE.MeshBasicMaterial({ color: 0xfffffff });
@@ -49,6 +77,77 @@ function createFloor() {
   plane.setRotationFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI / 2);
 
   scene.add(plane);
+}
+
+function createShogiBoard() {
+  const shogiBoardBox = new THREE.Mesh(SHOGI_BOARD_BOX_GEO, material);
+  shogiBoardBox.applyMatrix4(
+    translateMatrix(
+      0,
+      SHOGI_BOARD_BOX_GEO.boundingBox.max.y +
+        SHOGI_BOARD_LEG_GEO.boundingBox.max.y -
+        SHOGI_BOARD_LEG_GEO.boundingBox.min.y +
+        1.5 * SHOGI_BOARD_BOT_GEO.boundingBox.max.y,
+      0
+    )
+  );
+
+  const shogiBoardLegLeftBack = new THREE.Mesh(SHOGI_BOARD_LEG_GEO, material2);
+  shogiBoardLegLeftBack.applyMatrix4(
+    translateMatrix(
+      SHOGI_BOARD_BOX_GEO.boundingBox.min.x +
+        4 * SHOGI_BOARD_LEG_GEO.boundingBox.max.x,
+      SHOGI_BOARD_LEG_GEO.boundingBox.max.y +
+        1.5 * SHOGI_BOARD_BOT_GEO.boundingBox.max.y,
+      SHOGI_BOARD_BOX_GEO.boundingBox.min.z +
+        4 * SHOGI_BOARD_LEG_GEO.boundingBox.max.z
+    )
+  );
+
+  const shogiBoardLegRightBack = shogiBoardLegLeftBack.clone();
+  shogiBoardLegRightBack.applyMatrix4(reflectMatrix(-1, 1, 1));
+
+  const shogiBoardLegLeftFront = shogiBoardLegLeftBack.clone();
+  shogiBoardLegLeftFront.applyMatrix4(reflectMatrix(1, 1, -1));
+
+  const shogiBoardLegRightFront = shogiBoardLegLeftBack.clone();
+  shogiBoardLegRightFront.applyMatrix4(reflectMatrix(-1, 1, -1));
+
+  const shogiBoardBotLeftBack = new THREE.Mesh(SHOGI_BOARD_BOT_GEO, material3);
+  shogiBoardBotLeftBack.applyMatrix4(
+    translateMatrix(
+      SHOGI_BOARD_BOX_GEO.boundingBox.min.x +
+        4 * SHOGI_BOARD_LEG_GEO.boundingBox.max.x,
+      SHOGI_BOARD_BOT_GEO.boundingBox.max.y,
+      SHOGI_BOARD_BOX_GEO.boundingBox.min.z +
+        4 * SHOGI_BOARD_LEG_GEO.boundingBox.max.z
+    )
+  );
+
+  const shogiBoardBotRightBack = shogiBoardBotLeftBack.clone();
+  shogiBoardBotRightBack.applyMatrix4(reflectMatrix(-1, 1, 1));
+
+  const shogiBoardBotLeftFront = shogiBoardBotLeftBack.clone();
+  shogiBoardBotLeftFront.applyMatrix4(reflectMatrix(1, 1, -1));
+
+  const shogiBoardBotRightFront = shogiBoardBotLeftBack.clone();
+  shogiBoardBotRightFront.applyMatrix4(reflectMatrix(-1, 1, -1));
+
+  shogiBoard = new THREE.Group();
+
+  shogiBoard.add(shogiBoardBox);
+
+  shogiBoard.add(shogiBoardLegLeftBack);
+  shogiBoard.add(shogiBoardLegRightBack);
+  shogiBoard.add(shogiBoardLegLeftFront);
+  shogiBoard.add(shogiBoardLegRightFront);
+
+  shogiBoard.add(shogiBoardBotLeftBack);
+  shogiBoard.add(shogiBoardBotRightBack);
+  shogiBoard.add(shogiBoardBotLeftFront);
+  shogiBoard.add(shogiBoardBotRightFront);
+
+  scene.add(shogiBoard);
 }
 
 function animate() {
